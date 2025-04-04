3f962b09b051cf58a7a2cb6977ad127250910b1f
lopesboa <lopesboa1@gmail.com>
2025-04-03 21:30:20 -0300

    refactor: remove duplicated db user call

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -5,7 +5,7 @@ import { db } from "@/lib/db/db";
 import { auth } from "@/lib/auth";
 import { user } from "../db/schema";
 
-async function getUserFromSession() {
+export async function validateUserSession() {
 	try {
 		const session = await auth.api.getSession({
 			headers: await headers(),
@@ -21,39 +21,7 @@ async function getUserFromSession() {
 			return null;
 		}
 
-		return session;
-	} catch (error) {
-		return null;
-	}
-}
-
-export async function validateUserSession() {
-	try {
-		const session = await getUserFromSession();
-
-		if (!session) {
-			return null;
-		}
-
-		const userResult = await db.query.user.findFirst({
-			where: and(
-				eq(user.id, session.user.id),
-				isNull(user.deletedAt),
-				eq(user.emailVerified, true),
-			),
-			columns: {
-				id: true,
-				name: true,
-				email: true,
-				image: true,
-			},
-		});
-
-		if (!userResult) {
-			return null;
-		}
-
-		return userResult;
+		return session.user;
 	} catch (error) {
 		return null;
 	}

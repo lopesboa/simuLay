2cda309b9e683e8fb963d5bc85b79a5b88433cfd
lopesboa <lopesboa1@gmail.com>
2025-04-07 18:10:31 -0300

    refactor: wrape page with suspense

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,7 +4,7 @@ import { z } from "zod";
 import Link from "next/link";
 import Image from "next/image";
 import { toast } from "sonner";
-import { useActionState, useState } from "react";
+import { Suspense, useActionState, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { getZodConstraint, parseWithZod } from "@conform-to/zod";
 import { getFormProps, getInputProps, useForm } from "@conform-to/react";
@@ -43,57 +43,62 @@ export default function SignInPage() {
 	}
 
 	return (
-		<div className="card-border lg:min-w-[566px]">
-			<div className="flex flex-col gap-6 card py-14 px-10">
-				<div className="flex flex-row gap-2 justify-center">
-					<Image src="/logo.svg" alt="logo" height={32} width={38} />
-					<h2 className="text-primary-100">SimuLay</h2>
+		<Suspense>
+			<div className="card-border lg:min-w-[566px]">
+				<div className="flex flex-col gap-6 card py-14 px-10">
+					<div className="flex flex-row gap-2 justify-center">
+						<Image src="/logo.svg" alt="logo" height={32} width={38} />
+						<h2 className="text-primary-100">SimuLay</h2>
+					</div>
+
+					<h3>Practice job interviews with Simulay</h3>
+
+					<div>
+						<form
+							className="w-full space-y-6 mt-4 form"
+							action={action}
+							{...getFormProps(form)}
+						>
+							<Field
+								labelProps={{ children: "Email" }}
+								inputProps={{
+									...getInputProps(fields.email, {
+										type: "email",
+									}),
+									placeholder: "email@example.com",
+								}}
+								errors={fields.email.errors}
+							/>
+
+							<PasswordInput
+								labelProps={{ children: "Password" }}
+								showPassword={showPassword}
+								onClick={handleOnClick}
+								inputProps={{
+									...getInputProps(fields.password, {
+										type: showPassword ? "text" : "password",
+									}),
+									placeholder: "••••••••",
+								}}
+								errors={fields.password.errors}
+							/>
+
+							<SubmitButton className="btn" type="submit">
+								{"Sign In"}
+							</SubmitButton>
+						</form>
+					</div>
+					<p className="text-center">
+						{"No account yet?"}
+						<Link
+							href={"/sign-up"}
+							className="font-bold text-user-primary ml-1"
+						>
+							{"Sign Up"}
+						</Link>
+					</p>
 				</div>
-
-				<h3>Practice job interviews with Simulay</h3>
-
-				<div>
-					<form
-						className="w-full space-y-6 mt-4 form"
-						action={action}
-						{...getFormProps(form)}
-					>
-						<Field
-							labelProps={{ children: "Email" }}
-							inputProps={{
-								...getInputProps(fields.email, {
-									type: "email",
-								}),
-								placeholder: "email@example.com",
-							}}
-							errors={fields.email.errors}
-						/>
-
-						<PasswordInput
-							labelProps={{ children: "Password" }}
-							showPassword={showPassword}
-							onClick={handleOnClick}
-							inputProps={{
-								...getInputProps(fields.password, {
-									type: showPassword ? "text" : "password",
-								}),
-								placeholder: "••••••••",
-							}}
-							errors={fields.password.errors}
-						/>
-
-						<SubmitButton className="btn" type="submit">
-							{"Sign In"}
-						</SubmitButton>
-					</form>
-				</div>
-				<p className="text-center">
-					{"No account yet?"}
-					<Link href={"/sign-up"} className="font-bold text-user-primary ml-1">
-						{"Sign Up"}
-					</Link>
-				</p>
 			</div>
-		</div>
+		</Suspense>
 	);
 }

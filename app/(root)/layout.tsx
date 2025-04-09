13b0566460c9c6daa143b1ae0eb84fb98f1a4618
lopesboa <lopesboa1@gmail.com>
2025-04-09 17:03:41 -0300

    feat: use client root layout to enable user store

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import React, { type ReactNode } from "react";
 import { validateUserSession } from "@/lib/actions/auth.action";
 import { redirect } from "next/navigation";
+import { useUserStore, User } from "@/stores/user.store";
+import ClientRootLayout from "../client-layout";
 
 export default async function RootLayout({
 	children,
@@ -14,14 +16,16 @@ export default async function RootLayout({
 	}
 
 	return (
-		<div className="root-layout">
-			<nav>
-				<Link href="/" className="flex items-center gap-2">
-					<Image src="/logo.svg" alt="logo" width={32} height={38} />
-					<h2 className="text-primary-100">SimuLay</h2>
-				</Link>
-			</nav>
-			{children}
-		</div>
+		<ClientRootLayout user={userSesstion}>
+			<div className="root-layout">
+				<nav>
+					<Link href="/" className="flex items-center gap-2">
+						<Image src="/logo.svg" alt="logo" width={32} height={38} />
+						<h2 className="text-primary-100">SimuLay</h2>
+					</Link>
+				</nav>
+				{children}
+			</div>
+		</ClientRootLayout>
 	);
 }
